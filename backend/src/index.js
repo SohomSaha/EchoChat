3a75f117dc24d7646b0cacfe0749afdbc1422713
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2024-12-17 10:16:34 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,23 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import cors from "cors";
-
-import messageRoutes from "./routes/message.route.js";
-
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDb } from "./lib/db.js";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,10 +25,18 @@ app.use(
   })
 );
 
-//Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT);
-  connectDb();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
