77e5ffd095678d63de8f796037cab5cb786f5dc9
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2024-12-21 07:45:07 +0530

    Done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,7 +5,8 @@ export const generateToken = (userId,res) =>{
     });
     res.cookie("jwt",token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: "strict",
+        secure: true,
     });
 }
